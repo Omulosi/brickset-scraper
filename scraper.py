52125e4e4d9562fe5a3a30eeead66d664d9c9148
Omulosi <mulongojohnpaul@gmail.com>
2018-06-28 15:32:26 +0300

    add extra selectors and crawling functionality

--- scraper.py
+++ scraper.py
@@ -8,8 +8,23 @@ class BricksetSpider(scrapy.Spider):
     def parse(self, response):
         SET_SELECTOR = '.set'
         for brickset in response.css(SET_SELECTOR):
-            NAME_SELECTOR = 'h1 a ::text'
+            NAME_SELECTOR = 'h1 a span ::text'
+            PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
+            IMAGE_SELECTOR = 'img ::attr(src)'
             yield {
-                    'name': brickset.css(NAME_SELECTOR).extract_first()),
+                    'name': brickset.css(NAME_SELECTOR).extract_first(),
+                    'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
+                    'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
+                    'image': brickset.css(IMAGE_SELECTOR).extract_first(),
             }
+
+            NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
+            next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+            if next_page:
+                yield scrapy.Request(
+                        response.urljoin(next_page),
+                        callback=self.parse
+                        )
+
 
