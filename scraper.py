2568dc790b74dde475093693701843121f9b9c55
Omulosi <mulongojohnpaul@gmail.com>
2018-06-28 15:00:57 +0300

    add name selectors for each set

--- scraper.py
+++ scraper.py
@@ -3,5 +3,13 @@ import scrapy
 
 class BricksetSpider(scrapy.Spider):
     name = "brickset_spider"
-    start_urls = ['http://brickset.com/assets/year-2016']
+    start_urls = ['http://brickset.com/sets/year-2016']
+
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        for brickset in response.css(SET_SELECTOR):
+            NAME_SELECTOR = 'h1 a ::text'
+            yield {
+                    'name': brickset.css(NAME_SELECTOR).extract_first()),
+            }
 
